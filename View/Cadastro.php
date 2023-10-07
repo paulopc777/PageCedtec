6f0cd55c6fb6b46c50b91d66fbbcce14edd2bde9
PedruPeixot0 <147112228+PedruPeixot0@users.noreply.github.com>
2023-10-07 14:53:14 -0300

    Update Cadastro.php

--- View/Cadastro.php
+++ View/Cadastro.php
@@ -5,69 +5,60 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro alunos</title>
-    <style>
-        * {
-
-            margin: 0;
-            padding: 0;
-
-        }
-
-        .input-content {
-
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            flex-direction: column;
-
-        }
-    </style>
+    <link rel="stylesheet" href="cadastro.css">
 </head>
 
 <body>
 
-    <div class="form-content">
+    <main>
 
-        <form action="#">
-
-            <div class="input-content">
-
-                <div class="input_email">
-
-                    <label for="email" id="email" name="email">Email</label><br>
-
-                    <input type="text" id="email" name="email"><br><br>
-
-                </div>
-
-                <div class="input_senha">
-
-                    <label for="senha" id="senha" name="senha">Senha</label><br>
-
-                    <input type="password" id="senha" name="senha"><br><br>
-
-                </div>
-
-                <div class="input_cpf">
-
-                    <label for="cpf" id="cpf" name="cpf">Cpf</label><br>
-
-                    <input type="text" id="cpf" name="cpf"><br><br>
-
-                </div>
-
-                <div class="input_botao">
-
-                    <input type="submit">
-
-                </div>
-
-            </div>
-
-        </form>
-
+        <div class="img-content">
+            <img src="" alt="">
     </div>
+
+    <div class=" form-content">
+
+            <form action="#">
+
+                <div class="input-content">
+
+                    <div class="input_email">
+
+                        <label for="email" id="email" name="email"></label><br>
+
+                        <input type="text" id="email" name="email" placeholder="Email"><br><br>
+
+                    </div>
+
+                    <div class="input_senha">
+
+                        <label for="senha" id="senha" name="senha"></label><br>
+
+                        <input type="password" id="senha" name="senha" placeholder="Senha"><br><br>
+
+                    </div>
+
+                    <div class="input_cpf">
+
+                        <label for="cpf" id="cpf" name="cpf"></label><br>
+
+                        <input type="text" id="cpf" name="cpf" placeholder="CPF"><br><br>
+
+                    </div>
+
+                    <div class="btn-cadastro">
+
+                        <button>Cadastro</button>
+
+                    </div>
+
+                </div>
+
+            </form>
+
+        </div>
+
+    </main>
 
 </body>
 
