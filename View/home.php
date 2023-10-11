b87b2f1832501f714e4d50520e514548c4d7f53e
PedruPeixot0 <147112228+PedruPeixot0@users.noreply.github.com>
2023-10-10 21:50:14 -0300

    Create home.php

---
+++ View/home.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home page</title>
+    <link rel="stylesheet" href="">
+</head>
+<body>
+
+    <div class="menu">
+
+        <header>
+
+            <div class="box">
+
+                <a href="login.php">Login</a>
+                <a href="Cadastro.php">Cadastro</a>
+
+            </div>
+
+</header>
+
+</div>
+    
+</body>
+</html>
